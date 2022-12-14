57f1d4551d193497da4758a95ebed83dbfe763d1
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-14 15:42:31 +0100

    Initialising and Starting Code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// tailwind
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json'; 
+
+// navigation
+import { NavigationContainer } from '@react-navigation/native';
+
+//components
+import RootNavigator from './navigator/RootNavigator';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // @ts-ignore - TailwindProvider is missing a type definition
+    <TailwindProvider utilities={utilities}>
+        <NavigationContainer>      
+          <RootNavigator />
+        </NavigationContainer>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigator/TabNavigator.tsx
@@ -0,0 +1,65 @@
+/* rnfe - TabNavigator has 2 tabs: Customers and Orders -> can be changed in the bottom of the screen */
+import React, { useLayoutEffect } from "react";
+
+// navigation
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
+
+// screens
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+// icons
+import { Icon } from "@rneui/themed";
+
+// type definitions
+export type TabStackParamList = {
+  // will check if the names param are correct
+  Home: undefined;
+  Profile: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
+
+const TabNavigator = () => {
+  // removing the "Main" header from the RootNavigator
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "#59C1CC" : "gray"}
+              />
+            );
+          } else if (route.name === "Profile") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "#EB6A7C" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
