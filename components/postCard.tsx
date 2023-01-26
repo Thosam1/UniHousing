c59d3d8d0a01b41fab937aa771a1bb5507b332eb
Thosam1 <thosamtenzin2002@gmail.com>
2023-01-26 15:26:23 +0100

    Toast bug fixed

--- components/postCard.tsx
+++ components/postCard.tsx
@@ -9,7 +9,6 @@ import { Image } from "@rneui/themed";
 import { theme } from "../constants";
 import React from "react";
 import { Block, Button, Text } from ".";
-import { Toast } from "react-native-toast-message/lib/src/Toast";
 import {
   CompositeNavigationProp,
   useNavigation,
@@ -116,7 +115,6 @@ const PostCard = (props: PostCardProps) => {
           {props.price} CHF/month
         </Text>
       </TouchableOpacity>
-      <Toast />
     </View>
   );
 };
